88b8700d0509034a58b8d70d81af36609e81bdcc
GuillermoDo <125375520+GuillermoDo@users.noreply.github.com>
2023-02-14 13:03:52 -0300

    initial commit

---
+++ funcion.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){ irArriba(); }); 
+//ir arriba
+
+function irArriba(){
+  $('.ir-arriba').click(function(){ $('body,html').animate({ scrollTop:'0px' },600); });
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 0){ $('.ir-arriba').slideDown(600); }else{ $('.ir-arriba').slideUp(600); }
+  });
+  $('.ir-abajo').click(function(){ $('body,html').animate({ scrollTop:'1000px' },600); });
+}
