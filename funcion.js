50929012bdd06924da29e7c0061a21090cedebe7
GuillermoDo <125375520+GuillermoDo@users.noreply.github.com>
2023-02-14 17:17:20 -0300

    Update funcion.js

--- funcion.js
+++ funcion.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){ irArriba(); }); 
 //ir arriba
-
 function irArriba(){
   $('.ir-arriba').click(function(){ $('body,html').animate({ scrollTop:'0px' },600); });
   $(window).scroll(function(){
